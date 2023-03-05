6e3f9e16928f090ece53fef43a2d16e975b22d6e
YonatanFinerman <julianmode808@gmail.com>
2023-03-05 12:16:48 +0200

    more sections to home page

--- src/cmps/section-preview.jsx
+++ src/cmps/section-preview.jsx
@@ -1,9 +1,10 @@
 import { RiArrowDownSLine } from "react-icons/ri";
+
 export function SectionPreview({ carSectionInfo }) {
 
     const { title, description, imgUrl, btn1Txt, btn2Txt } = carSectionInfo
 
-    return <section className="home-hero">
+    return <section className="home-hero" style={{ backgroundImage: `url(${require(`../assets/img/${imgUrl}.jpg`)})` }}>
         <header>
             <h1>{title}</h1>
             <p>{description}</p>
@@ -12,9 +13,9 @@ export function SectionPreview({ carSectionInfo }) {
         <div>
             <div className="btn-group">
                 <button className="custom-order-btn">{btn1Txt}</button>
-                <button className="inventory-btn">{btn2Txt}</button>
+                {(btn2Txt) && <button className="inventory-btn">{btn2Txt}</button>}
             </div>
-            {(title === 'Model S') && <div className="down-btn"><RiArrowDownSLine /></div>}
+            {(title === 'Model S') && <div className="down-btn" ><RiArrowDownSLine /></div>}
 
         </div>
     </section>
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,16 +1,39 @@
 import { SectionsList } from "../cmps/section-list"
 
-const sectionsInfo = [{title:'Model S',description:'Order online for touchless delivery',
-imgUrl:'model-l.jpg',btn1Txt:'Custom order',btn2Txt:'Existing inventory'},
-{title:'Model l',description:'Order online for touchless delivery',
-imgUrl:'model-s.jpg',btn1Txt:'Custom order',btn2Txt:'Existing inventory'},
-{title:'Model S',description:'Order online for touchless delivery',
-imgUrl:'model-s.jpg',btn1Txt:'Custom order',btn2Txt:'Existing inventory'}]
+const sectionsInfo = [{
+    title: 'Model S', description: 'Order online for touchless delivery',
+    imgUrl: 'model-s', btn1Txt: 'Custom order', btn2Txt: 'Existing inventory'
+},
+{
+    title: 'Model Y', description: 'Order online for touchless delivery',
+    imgUrl: 'model-y', btn1Txt: 'Custom order', btn2Txt: 'Existing inventory'
+},
+{
+    title: 'Model 3', description: 'Order online for touchless delivery',
+    imgUrl: 'model-3', btn1Txt: 'Custom order', btn2Txt: 'Existing inventory',
+},
+{
+    title: 'Model X', description: 'Order online for touchless delivery',
+    imgUrl: 'model-x', btn1Txt: 'Custom order', btn2Txt: 'Existing inventory',
+},
+{
+    title: 'Lowest cost solar panels in america', description: 'Money-back guarantee',
+    imgUrl: 'solar-panel', btn1Txt: 'Order now', btn2Txt: 'Learn more',
+},
+{
+    title: 'Solar for new roofs', description: 'Solar roof costs less than a new roof plus solar panels',
+    imgUrl: 'solar-roof', btn1Txt: 'Order now', btn2Txt: 'Learn more',
+},
+{
+    title: 'Accessories', description: '',
+    imgUrl: 'accessories', btn1Txt: 'Shop now',
+},
+]
 
-export function HomePage(){
+export function HomePage() {
     return <section className="home-page">
 
-        <SectionsList sectionsInfo={sectionsInfo}/>
-      
+        <SectionsList sectionsInfo={sectionsInfo} />
+
     </section>
 }
