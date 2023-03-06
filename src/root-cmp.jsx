28585a1b7cc7634b36fe03dd490b8ab66d77c882
YonatanFinerman <julianmode808@gmail.com>
2023-03-06 10:52:42 +0200

    added header sections

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -8,7 +8,7 @@ import { HomePage } from './pages/home-page';
 function App() {
   return (
     <div className="App">
-     {/* <AppHeader/> */}
+     <AppHeader/>
      <HomePage/>
     </div>
   );
